238eeb882309f586d0b80079746ad686302de88d
YuraBarykin <yBarykin@gmail.com>
2024-08-07 18:57:50 +0400

    Настройка проекта vite + react + ts + eslint + prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,23 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'vite.config.ts'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: ["./tsconfig.app.json"],
+  },
+  plugins: ['react-refresh'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+    "react/react-in-jsx-scope": 0
+  },
+}
