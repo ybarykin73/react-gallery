744d954cc08b5f9b056c9273d1214abb7dcb72e7
YuraBarykin <yBarykin@gmail.com>
2024-08-18 13:08:28 +0400

    Рефакторинг аккардиона

--- src/components/subcomponents/accordion/Accordion.tsx
+++ src/components/subcomponents/accordion/Accordion.tsx
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 
 import styles from './style.module.scss';
 import { IProps } from './IAccordion';
 
 const Accordion: React.FC<IProps> = (props) => {
   const { name, children } = props;
+
+  const wrapper = useRef(null);
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <label className={styles.label}>
-      <div className={styles.title}>{name}</div>
-      <input className={styles.input} type="checkbox" />
-      <div className={styles.body}>{children}</div>
-      <span className={styles.decore} />
-    </label>
+    <div
+      ref={wrapper}
+      className={`${styles.wrapper} ${isOpen ? styles.active : 'active'}`}
+    >
+      <button
+        type="button"
+        aria-label="Open"
+        className={styles.button}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        <div className={styles.title}>{name}</div>
+        <span className={styles.decore} />
+      </button>
+      <div className={styles.body}>
+        <div className={styles.main}>{children}</div>
+      </div>
+    </div>
   );
 };
 
